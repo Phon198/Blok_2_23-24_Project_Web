f7038c384b8c538884741876f7257b80ccd2f02a
Phon198 <150865805+Phon198@users.noreply.github.com>
2023-12-06 14:17:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
-// JavaScript Document
-console.log("hi");
+// Hamburger Menu Styles
+document.addEventListener("DOMContentLoaded", function () {
+    var openButton = document.getElementById("menu-toggle");
+    var closeButton = document.getElementById("close-menu");
+    var nav = document.querySelector("nav");
+
+    openButton.addEventListener("click", function () {
+        nav.classList.add("show-menu");
+    });
+
+    closeButton.addEventListener("click", function () {
+        nav.classList.remove("show-menu");
+    });
+});
