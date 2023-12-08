0164ffd12fb74950f38a7a4b8ffcb9e4747fd403
Phon198 <150865805+Phon198@users.noreply.github.com>
2023-12-08 03:06:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Hamburger Menu Styles
+// Hamburger Menu
 document.addEventListener("DOMContentLoaded", function () {
     var openButton = document.getElementById("menu-toggle");
     var closeButton = document.getElementById("close-menu");
@@ -12,3 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
         nav.classList.remove("show-menu");
     });
 });
+
+//Scearch Bar 
+// https://codepen.io/shooft/pen/JjxmvrL
+const searchButton = document.querySelector("header > button:nth-of-type(2)");
+const searchSection = document.querySelector("header > search");
+
+searchButton.onclick = toggleSearch;
+
+function toggleSearch() {
+	searchSection.classList.toggle("open");
+}
